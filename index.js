1c0aace339b12d76c8cd64882fbfd3d2428c08c2
Ahmad Al Khaldi <khalidi86@yahoo.com>
2019-12-04 05:57:44 +0000

    Added Object access

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("test");
+//////////// Object simple loops /////////////
+let car = {
+  make: 'Ford',
+  model: 'Mustang',
+  year: 2020,
+  color: 'Black'
+}
+// car.make
+// console.log(Object.keys(car));
+// console.log(Object.values(car));
+// Object.entries(car); // Array of each key/value 
+
+// Log the values in a loop
+// Object.values(car).forEach(element => {
+//   console.log(element);
+// })
+
+
+// Access a value of some property 
+// car['make']
+
+
+// for in loop to log each prop and its Value 
+// for (let prop in car) {
+//   console.log(prop + ": " + car[prop]);
+// }
+
+// for each loop to log each prop and its Value since Object.keys creates an array
+// let carKeys = Object.keys(car);
+// carKeys.forEach(element => {
+//   console.log(element + ": " + car[element])
+// });
+
+
+
+///////////// Arrays Simple Loops ///////////////
